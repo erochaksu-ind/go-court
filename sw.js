1f1208751af4b4869f95961a3bd7d4a858c11c28
erochaksu-ind <erochaksuelection@gmail.com>
2026-09-13 13:08:12 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'court-daily-vue-v2'
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
+    const scope = self.registration.scope
+    return cache.addAll([
+      scope,
+      new URL('index.html', scope).toString(),
+      new URL('manifest.webmanifest', scope).toString(),
+    ])
+  }))
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))))
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+          const copy = response.clone()
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy))
+        }
+        return response
+      })
+      .catch(async () => {
+        const cached = await caches.match(event.request)
+        return cached || caches.match(new URL('index.html', self.registration.scope).toString())
+      }),
+  )
+})
